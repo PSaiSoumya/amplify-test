063fa1afec58a19149bbfd8425a6ac598bf2f257
Soumya <128458176+PSaiSoumya@users.noreply.github.com>
2024-04-24 16:27:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { Amplify } from 'aws-amplify';
-import './App.css';
-import MaintenancePage from "./MaintenancePage.js"
+// App.js
 
-function App() {
-  const [isMaintenance, setIsMaintenance] = useState(false);
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MaintenancePage from './MaintenancePage'; // Import the MaintenancePage component
 
-  useEffect(() => {
-    const checkMaintenanceMode = async () => {
-      const config = await Amplify.configure();
-      const maintenanceMode = config.runtimeConfig.env.REACT_APP_MAINTENANCE_MODE;
-      setIsMaintenance(maintenanceMode === 'true');
-    };
-    checkMaintenanceMode();
-  }, []);
-
+const App = () => {
   return (
-    <div className="App">
-      {isMaintenance ? (
-        <MaintenancePage />
-      ) : (
-        <div>
-          <h1>Your Application Content Test</h1>
-          {/* Your application content goes here */}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/maintenance" component={MaintenancePage} />
+        {/* Other routes */}
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;

a952197a769b01dea87e8e7d7b8a0c14b3eb4164
Soumya <128458176+PSaiSoumya@users.noreply.github.com>
2024-04-24 16:49:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MaintenancePage from './MaintenancePage'; // Import the MaintenancePage component
 
 const App = () => {
